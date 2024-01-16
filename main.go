9c785b5cc2dc0671830469a08af2c1d095b0e2cd
DrSteal <inessarylog226@gmail.com>
2024-01-16 14:09:53 +0300

    init: main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	// a := [5]int{1, 2, 3, 7, 91}
+	// b := [5]int{3, 8, 9, 12, 28}
+	// c := []int{}
+
+	// for _, n1 := range a {
+	// 	for _, n2 := range b {
+	// 		if n1 > n2 {
+
+	// 		}
+	// 	}
+	// }
+
+}
