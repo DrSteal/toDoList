4d5f6d48b0ffc4b0ce2832e5690bbd30bdf81814
DrSteal <inessarylog226@gmail.com>
2024-04-15 13:56:49 +0300

    init: add api and

--- main.go
+++ main.go
@@ -1,16 +1,66 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type ExchLen struct {
+	Exch float64 `json:"Value"`
+}
+
+type AZN struct {
+	Currency ExchLen `json:"AZN"`
+}
+
+type Response struct {
+	Valute AZN `json:"Valute"`
+}
+
+// type Product struct {
+// 	Name  string
+// 	Price int
+// }
+
+// var products = []Product{
+// 	{
+// 		Name:  "Milk",
+// 		Price: 4200,
+// 	},
+// 	{
+// 		Name:  "Coffe",
+// 		Price: 5000,
+// 	},
+// }
+
 func main() {
-	// a := [5]int{1, 2, 3, 7, 91}
-	// b := [5]int{3, 8, 9, 12, 28}
-	// c := []int{}
+	url := "https://www.cbr-xml-daily.ru/daily_json.js"
 
-	// for _, n1 := range a {
-	// 	for _, n2 := range b {
-	// 		if n1 > n2 {
+	resp, err := http.Get(url)
 
-	// 		}
-	// 	}
-	// }
+	if err != nil {
+		panic(err)
+	}
+
+	var response Response
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.Valute.Currency.Exch)
 
 }
+
+// http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
+// 	err := json.NewEncoder(w).Encode(products)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// })
+
+// log.Println("server start")
+// http.ListenAndServe(":8000", nil)
