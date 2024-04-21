e66e958fc6adcb90fdc1cecfb8e8fd0f44898213
DrSteal <inessarylog226@gmail.com>
2024-04-21 12:25:04 +0300

    init: add go.mod

--- main.go
+++ main.go
@@ -1,66 +1,109 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type ExchLen struct {
-	Exch float64 `json:"Value"`
-}
-
-type AZN struct {
-	Currency ExchLen `json:"AZN"`
-}
-
-type Response struct {
-	Valute AZN `json:"Valute"`
-}
-
-// type Product struct {
-// 	Name  string
-// 	Price int
+// type ExchLen struct {
+// 	Exch float64 `json:"Value"`
 // }
 
-// var products = []Product{
-// 	{
-// 		Name:  "Milk",
-// 		Price: 4200,
-// 	},
-// 	{
-// 		Name:  "Coffe",
-// 		Price: 5000,
-// 	},
+// type AZN struct {
+// 	Currency ExchLen `json:"AZN"`
 // }
+
+// type Response struct {
+// 	Valute AZN `json:"Valute"`
+// }
+
+type Product struct {
+	Name  string
+	Price int
+}
+
+var products = []Product{
+	{
+		Name:  "milk",
+		Price: 10000,
+	},
+	{
+		Name:  "tea",
+		Price: 80000,
+	},
+}
+
+func createTable(db *sql.DB) {
+	_, err := db.Exec(`
+		CREATE TABLE  product (
+			name text,
+			price integer
+		)
+	`)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	url := "https://www.cbr-xml-daily.ru/daily_json.js"
 
-	resp, err := http.Get(url)
-
-	if err != nil {
-		panic(err)
-	}
-
-	var response Response
-
-	err = json.NewDecoder(resp.Body).Decode(&response)
+	db, err := sql.Open("sqlite3", "product.db")
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(response.Valute.Currency.Exch)
+	createTable(db)
+
+	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
+
+		rows, err := db.Query("SELECT name, price FROM product ORDER BY name")
+
+		if err != nil {
+			log.Println("query products error,", err)
+			return
+		}
+
+		var productList []Product
+
+		for rows.Next() {
+			var p Product
+			if err := rows.Scan(&p.Name, &p.Price); err != nil {
+				log.Println("scan products error,", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			productList = append(productList, p)
+
+		}
+
+		json.NewEncoder(w).Encode(productList)
+	})
+
+	log.Println("start server")
+	http.ListenAndServe(":8000", nil)
+
+	// 	url := "https://www.cbr-xml-daily.ru/daily_json.js"
+
+	// 	resp, err := http.Get(url)
+
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	// 	var response Response
+
+	// 	err = json.NewDecoder(resp.Body).Decode(&response)
+
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	// 	fmt.Println(response.Valute.Currency.Exch)
 
 }
-
-// http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
-// 	err := json.NewEncoder(w).Encode(products)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// })
-
-// log.Println("server start")
-// http.ListenAndServe(":8000", nil)
